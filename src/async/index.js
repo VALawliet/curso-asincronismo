33f912ef453af8f1cd2dd54615af72d9c1eb998a
Law <victorestrada2006@gmail.com>
2023-05-12 16:25:59 +0200

    Modificando un archivo para que siga funcionando de forma correcta

--- src/async/index.js
+++ src/async/index.js
@@ -1,2 +1,24 @@
 'use strict'
 
+const fnAsync = ()=>{
+    return new Promise((resolve, reject)=>{
+        if(true){
+            setTimeout(()=>{
+                resolve('Hey')
+            }, 2000);
+
+        }else{
+            reject(new Error('Error, L'));
+        }
+    })
+}
+
+const anotherFnc = async ()=>{
+    const something = await fnAsync();
+    console.log(something);
+    console.log('HEyyyy')
+}
+
+console.log('Before')
+anotherFnc()
+console.log('after')
